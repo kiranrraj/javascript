9f7670b14357ff0dc93be307c94cfa8d92c24676
Kiran Raj R <kiranrajvjd@gmail.com>
2021-01-05 20:51:03 +0530

    Day_2 all issues fixed

--- Day_2_04.01.2021/day_2_js.js
+++ Day_2_04.01.2021/day_2_js.js
@@ -6,18 +6,18 @@ const btn_down = document.querySelector('.button__down');
 const s_length = slider_left.querySelectorAll('div').length;
 
 let index = 0
+slider_left.style.top = `-${(s_length - 1) * 100}vh`;
 btn_up.addEventListener('click', ()=> moveSlide('up'));
 btn_down.addEventListener('click', ()=> moveSlide('down'));
 
 function moveSlide(direction){
     const slide_height = slider.clientHeight;
     if (direction == 'up'){
-        slider_left.style.top = `-${(s_length - index) * 100}vh`;
         index++;
         // console.log("up", slider_left.style.top, index)
-        if (index > s_length){
+        if (index > s_length-1){
             index = 0
-            // console.log("up", slider_left.style.top, index)
+            console.log("up", slider_left.style.top, index)
         }
     }else if(direction == 'down'){
         index--;
@@ -31,5 +31,5 @@ function moveSlide(direction){
     //     slider_right.style.transform = `translateY(-${index * slide_height}px)`;
    
     slider_right.style.transform = `translateY(-${index * slide_height}px)`;
-    slider_right.style.transform = `translateY(-${index * slide_height}px)`;
+    slider_left.style.transform = `translateY(${index * slide_height}px)`;
 }
