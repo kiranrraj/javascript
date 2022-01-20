0b54297f64f7239cb29e81e7adadd8116989c9ba
Kiran Raj R <kiranrajvjd@gmail.com>
2022-01-20 23:44:31 +0530

    --Project 25: --Flash Card Generator
    A flash card generator using JavaScript, CSS and HTML
    
    -------------------------------
    Project: Notification Generator
    Author: Kiran Raj R
    Last Update: 20/01/2022
    Technologies: HTML, CSS, JavaScript
    Project Status: Pending
    --------------------------------

---
+++ Project_25_flashCard/p25_main.js
@@ -0,0 +1,6 @@
+{/* <div class="flashCard">
+    <h3 class="flashCard--heading">Flash Card</h3>
+    <p class="flashCard--question"></p>
+    <p class="flashCard--answer"></p>
+    <button class="flashCard--btn">Show Answer</button>
+</div> */}
